317425ad670f98455a9b4e9e400e43f8f716d9b4
Maksym <134280265+BazylevychInhale@users.noreply.github.com>
2024-07-03 22:10:13 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { CHAIN, TonConnectButton } from "@tonconnect/ui-react";
 import { useTonConnect } from "./hooks/useTonConnect";
-import "@twa-dev/sdk"
+import "@twa-dev/sdk";
 
 const StyledApp = styled.div`
   background-color: #e8e8e8;
@@ -24,14 +24,19 @@ const AppContainer = styled.div`
 `;
 
 function App() {
-  const {network} = useTonConnect()
+  const { network } = useTonConnect();
+
+  const handleDeepLink = () => {
+    window.location.href =
+      "https://app.tonkeeper.com/ton-connect?id=fe19613be01574d5615afc60027d1e6bdaab4946c23a4c75503ba8ab4204d901&open=1&r=%7B%22manifestUrl%22%3A%22https%3A%2F%2Fraw.githubusercontent.com%2FBazylevych%2Fton-app%2Fmain%2Fpublic%2Ftonconnect-manifest.json%22%2C%22items%22%3A%5B%7B%22name%22%3A%22ton_addr%22%7D%5D%7D&ret=back&v=2";
+  };
 
   return (
     <StyledApp>
       <AppContainer>
         <FlexBoxCol>
           <FlexBoxRow>
-            <TonConnectButton/>
+            <TonConnectButton />
             <Button>
               {network
                 ? network === CHAIN.MAINNET
@@ -41,6 +46,9 @@ function App() {
             </Button>
           </FlexBoxRow>
           <Jetton />
+          <div>
+            <button onClick={handleDeepLink}>Deeplink</button>
+          </div>
         </FlexBoxCol>
       </AppContainer>
     </StyledApp>
